94afcbed5a20a7be9644c3ed30b04d317bb019ba
Shiqi Wang <thisisshiki@outlook.com>
2024-12-12 19:39:25 +1100

    fix link

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/odin-shopping-cart/home',
+  base: '/odin-shopping-cart/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
